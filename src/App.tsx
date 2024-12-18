8e8c2374372b96e222de29e2d77d7babbdb8d3e2
Lucas Justino <justinolucas@hotmail.com>
2024-12-18 12:13:32 -0300

    Finished update

--- src/App.tsx
+++ src/App.tsx
@@ -5,29 +5,56 @@ import { mangaData } from './interface/mangaData';
 import { useMangaData } from './hooks/useMangaData'
 import { useState } from 'react';
 import { CreateModal } from './components/create-modal/create-modal'
+import { UpdateModal } from './components/update-modal/update-modal';
 
 function App() {
-  const {data} = useMangaData();
+  const { data } = useMangaData();
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isUpdateModalOpen, setIsUpdateModalOpen] = useState(false);
+  const [selectedManga, setSelectedManga] = useState<mangaData | null>(null);
 
   const handleOpenModal = () => {
-    setIsModalOpen(prev => !prev)
+    setIsModalOpen(true);
   }
-  
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  }
+
+  const handleOpenUpdateModal = (manga: mangaData) => {
+    setSelectedManga(manga);
+    setIsUpdateModalOpen(true);
+  }
+
+  const handleCloseUpdateModal = () => {
+    setSelectedManga(null);
+    setIsUpdateModalOpen(false);
+  }
+
+  const sortedData = data?.sort((a, b) => a.nome.localeCompare(b.nome));
+
   return (
-    <div className='container'>
+    <div className="container">
       <h1>Lista de Mangas</h1>
-      <div className='card-grip'>
-        {data?.map(mangaData => 
-        <Card key={mangaData.id}
-        nome={mangaData.nome}
-        imagem_capa={mangaData.imagem_capa}
-        capitulos={mangaData.capitulos}/>)}
-        {isModalOpen && <CreateModal closeModal={handleOpenModal}/>}
-        <button onClick={handleOpenModal}>novo</button>
+      <div className="card-grip">
+        {sortedData?.map((mangaData) => (
+          <div key={mangaData.id} className="card">
+            <Card
+              nome={mangaData.nome}
+              imagem_capa={mangaData.imagem_capa}
+              capitulos={mangaData.capitulos}
+            />
+            <button onClick={() => handleOpenUpdateModal(mangaData)}>
+              Atualizar
+            </button>
+          </div>
+        ))}
+        {isModalOpen && <CreateModal closeModal={handleCloseModal} />}
+        {isUpdateModalOpen && selectedManga && (<UpdateModal closeModal={handleCloseUpdateModal} manga={selectedManga} />)}
+        <button onClick={handleOpenModal} className='novo-produto-button'>Novo</button>
       </div>
     </div>
-  )
+  );
 }
 
 export default App
