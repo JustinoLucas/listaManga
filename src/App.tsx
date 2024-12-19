4453580bfd27fcba2451b1f4b98483df2ea844b9
Lucas Justino <justinolucas@hotmail.com>
2024-12-19 12:52:34 -0300

    Atualizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
 import './App.css'
 import './components/card/card.css'
 import { Card } from './components/card/card'
-import { mangaData } from './interface/mangaData';
 import { useMangaData } from './hooks/useMangaData'
 import { useState } from 'react';
 import { CreateModal } from './components/create-modal/create-modal'
-import { UpdateModal } from './components/update-modal/update-modal';
-import { DeleteModal } from './components/delete-modal/delete-modal';
-import { useMangaDataDelete } from './hooks/useMangaDataExclude';
 
 function App() {
   const { data } = useMangaData();
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [isUpdateModalOpen, setIsUpdateModalOpen] = useState(false);
-  const [selectedManga, setSelectedManga] = useState<mangaData | null>(null);
-  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
-  const [selectedMangaId, setSelectedMangaId] = useState<number | null>(null);
-  const [selectedMangaName, setSelectedMangaName] = useState<string | null>(null);
-  const { mutate: deleteManga } = useMangaDataDelete();
 
   const handleOpenModal = () => {
     setIsModalOpen(true);
@@ -27,33 +17,6 @@ function App() {
     setIsModalOpen(false);
   }
 
-  const handleOpenUpdateModal = (manga: mangaData) => {
-    setSelectedManga(manga);
-    setIsUpdateModalOpen(true);
-  }
-
-  const handleCloseUpdateModal = () => {
-    setSelectedManga(null);
-    setIsUpdateModalOpen(false);
-  }
-
-  const handleOpenDeleteModal = (id: number, name: string) => {
-    setSelectedMangaId(id);
-    setSelectedMangaName(name);
-    setIsDeleteModalOpen(true);
-  };
-
-  const handleCloseDeleteModal = () => {
-    setSelectedMangaId(null);
-    setIsDeleteModalOpen(false);
-  };
-
-  const handleConfirmDelete = () => {
-    if (selectedMangaId !== null) {
-      deleteManga(selectedMangaId);
-    }
-    handleCloseDeleteModal();
-  };
 
   const sortedData = data?.sort((a, b) => a.nome.localeCompare(b.nome));
 
@@ -69,19 +32,9 @@ function App() {
               imagem_capa={mangaData.imagem_capa}
               capitulos={mangaData.capitulos}
             />
-            <div className='botoes-card'>
-              <button onClick={() => handleOpenUpdateModal(mangaData)} className='atualizar-manga-button'>
-                Atualizar
-              </button>
-              <button onClick={() => handleOpenDeleteModal(mangaData.id, mangaData.nome)} className="delete-manga-button">
-                Excluir
-              </button>
-            </div>
           </div>
         ))}
         {isModalOpen && <CreateModal closeModal={handleCloseModal} />}
-        {isUpdateModalOpen && selectedManga && (<UpdateModal closeModal={handleCloseUpdateModal} manga={selectedManga} />)}
-        {isDeleteModalOpen && (<DeleteModal closeModal={handleCloseDeleteModal} confirmDelete={handleConfirmDelete} mangaName={selectedMangaName}/>)}
         <button onClick={handleOpenModal} className='novo-manga-button'>Adicionar Manga</button>
       </div>
     </div>
